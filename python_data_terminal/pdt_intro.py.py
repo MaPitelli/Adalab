82fe07ab572ba92df839eeb1ba0a7019a95f63b4
MaPitelli <mairapitelli@hotmail.com>
2024-05-28 09:05:57 +0200

    python_data_terminal_intro started

---
+++ python_data_terminal/pdt_intro.py.py
@@ -0,0 +1,23 @@
+'''Cursos de Python, Data y Terminal
+
+Cursos Recomendables.
+Esta sección es una de las más importantes de la semana previa de trabajo. 
+Os facilitamos unos cursos para que podáis comprender los diferentes roles que existen en el mundo data, como trabajar con Jupyter Notebook y otro para empezar a enfrentarse a terminal y la línea de comandos.
+
+Estudiar y comprender esta información básica os ayudará a entender desde el día 0 vuestro rol como futuras analistas de datos.
+
+Entendemos que esto puede ser un desafío, ¡pero no te preocupes!, todos los conocimientos que contienen estos cursos los vamos a explicar en profundidad en el curso.
+
+Como alumna del bootcamp de Data Analytics tendrás acceso gratuito a la plataforma de Platzi donde encontrarás los siguientes cursos:
+
+Curso Introductorio mundo data: link > https://platzi.com/cursos/aprender-data-ia/
+
+Introducción a la Terminal y Línea de Comandos: link > https://platzi.com/cursos/terminal/
+
+Curso Básico de Python: link > https://platzi.com/cursos/python/
+
+Curso de Entorno de Trabajo para Ciencia de Datos con Jupyter Notebooks y Anaconda: link > https://platzi.com/cursos/jupyter-notebook/
+
+Si tienes problemas a la hora de crear tu cuenta o de acceder a algunos de los cursos, ponte en contacto con el equipo de Platzi para que te puedan ayudarte. 
+Pueden tardar algunos días en darte de alta. Si al cabo de una semana aún no tienes acceso a la plataforma y tampoco encuentras el correo en la carpeta de Spam. 
+Por favor, ponte en contacto con el equipo de Platzi indicando que eres Adalaber.'''
