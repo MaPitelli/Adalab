16ee00fa8e0563d1f2c7d85d60dc20e76dd2024a
MaPitelli <mairapitelli@hotmail.com>
2024-06-05 19:47:40 +0200

    Módulo 1, lección 02 Tuplas concluído. Módulo 0 Agile concluído.

--- modulo_0/agile.py
+++ modulo_0/agile.py
@@ -40,17 +40,26 @@ Resumiendo, Agile es una cultura que trasciende a toda la organización, que est
 Todo esto se aterriza a través de un número ilimitado de prácticas como Design Thinking, Kanban, eXtreme Programming (XP) y Scrum que ayudan a su consecución.
 
 
-Para ello, hablaremos primero del conocido Ciclo de Deming, el Plan Do Check Act (PDCA): planifica, haz, comprueba y actúa, y vuelta a empezar. Edward Deming fue un reputado estadista del siglo pasado que introdujo, mucho antes del nacimiento de Agile, la utilidad de trabajar de forma cíclica. Él ya se aventuraba a asegurar que trabajar mediante ciclos de inspección y adaptación era una forma eficaz de ir aprendiendo.
+Para ello, hablaremos primero del conocido Ciclo de Deming, el Plan Do Check Act (PDCA): planifica, haz, comprueba y actúa, y vuelta a empezar. 
+Edward Deming fue un reputado estadista del siglo pasado que introdujo, mucho antes del nacimiento de Agile, la utilidad de trabajar de forma cíclica. 
+Él ya se aventuraba a asegurar que trabajar mediante ciclos de inspección y adaptación era una forma eficaz de ir aprendiendo.
 
-Y, ¿cómo funciona? Pues la idea es planificar qué hacer durante un periodo corto, a continuación comenzar hacerlo, y cuando acabe, medir qué tal ha salido para, finalmente, valorar los puntos de acción sobre el plan para ir mejorando y volver a iterar. De hecho, la teoría empírica o método científico basado en la experimentación se basa en esto. Así, el típico "prueba y error" podría corresponderse con los ciclos de Deming que se repetirían una y otra vez hasta ir alcanzando la mejor aproximación. Y aquí es donde entra en juego Scrum.
+Y, ¿cómo funciona? Pues la idea es planificar qué hacer durante un periodo corto, a continuación comenzar hacerlo, y cuando acabe, medir qué tal ha salido para, finalmente, 
+valorar los puntos de acción sobre el plan para ir mejorando y volver a iterar. De hecho, la teoría empírica o método científico basado en la experimentación se basa en esto. 
+Así, el típico "prueba y error" podría corresponderse con los ciclos de Deming que se repetirían una y otra vez hasta ir alcanzando la mejor aproximación. Y aquí es donde entra en juego Scrum.
 
-Scrum nace como un marco de prácticas ágiles orientadas al desarrollo. Scrum establece objetivos a corto plazo y, sistemáticamente, paso a paso, va encontrando la manera de llegar a ellos a través de una serie de eventos y de roles. Es lo que se llama un proceso iterativo e incremental, acorde al Ciclo de Deming anteriormente mencionado y basándose en pequeños ciclos de desarrollo, que serán cortos periodos de tiempo previamente establecido, fijo y acotado que llamaremos Sprints. Pero no os adelanteremos nada más... tendréis que leer la siguiente guía para profundizar en cómo vamos a hacerlo.
+Scrum nace como un marco de prácticas ágiles orientadas al desarrollo. Scrum establece objetivos a corto plazo y, sistemáticamente, paso a paso, va encontrando la manera de llegar a ellos a través de una serie de eventos 
+y de roles. Es lo que se llama un proceso iterativo e incremental, acorde al Ciclo de Deming anteriormente mencionado y basándose en pequeños ciclos de desarrollo, 
+que serán cortos periodos de tiempo previamente establecido, fijo y acotado que llamaremos Sprints. Pero no os adelanteremos nada más... tendréis que leer la siguiente guía para profundizar en cómo vamos a hacerlo.
 
 ¿Por qué Agile en Adalab?
-Introducir Agile en el momento en el momento previo a comenzar la formación es clave ya que permite dotar de información y herramientas para una mejor gestión del nuevo contexto que, como ya hemos comentado, es bastante cambiante. Y, por ello, queremos que las alumnas de Adalab entiendan el porqué de este mindset desde el primer día. Así, gracias a habilidades como trabajar en equipo, comunicarse de forma efectiva, la tolerancia al fallo, etc. os convertiréis en grandísimas profesionales desde el primer momento, facilitando vuestra incorporación a cualquier empresa tecnológica, comprendiendo la cultura organizativa y adaptándose al nuevo entorno de forma más eficiente, lo cual marcará la diferencia respecto a otros perfiles juniors.
+Introducir Agile en el momento previo a comenzar la formación es clave ya que permite dotar de información y herramientas para una mejor gestión del nuevo contexto que, como ya hemos comentado, es bastante cambiante. 
+Y, por ello, queremos que las alumnas de Adalab entiendan el porqué de este mindset desde el primer día. Así, gracias a habilidades como trabajar en equipo, comunicarse de forma efectiva, la tolerancia al fallo, etc. 
+os convertiréis en grandísimas profesionales desde el primer momento, facilitando vuestra incorporación a cualquier empresa tecnológica, comprendiendo la cultura organizativa y adaptándose al nuevo entorno de forma más 
+eficiente, lo cual marcará la diferencia respecto a otros perfiles juniors.
 
-Además, el propio Adalab se centra en esa cultura de empresa. Prueba de ello es que la gestión de Adalab responde a este concepto: se busca la mejora constante a través del feedback de las alumnas, profesores/as, facilitadores/as, voluntarios/as, empresas colaboradoras… para ir adaptando la formación a lo realmente demandado, poniendo foco en las personas que lo hacen posible y que aportan un valor añadido para crear un bootcamp diferenciador como este.
-
-Para saber cómo lo trabajaremos en Adalab, podéis seguir con la siguiente sección de la guía de Scrum donde lo contaremos en detalle.
+Además, el propio Adalab se centra en esa cultura de empresa. Prueba de ello es que la gestión de Adalab responde a este concepto: se busca la mejora constante a través del feedback de las alumnas, 
+profesores/as, facilitadores/as, voluntarios/as, empresas colaboradoras… para ir adaptando la formación a lo realmente demandado, poniendo foco en las personas que lo hacen posible y que aportan un valor añadido 
+para crear un bootcamp diferenciador como este.
 
 '''
