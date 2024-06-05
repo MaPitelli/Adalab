4825063556fd9a832b46046723e43a604afb3d1a
MaPitelli <mairapitelli@hotmail.com>
2024-06-05 18:24:31 +0200

    Módulo 1, lección 02 'Listas' concluído. Estudiando Agile.

---
+++ modulo_0/agile.py
@@ -0,0 +1,56 @@
+'''
+Agile
+
+Durante el programa, además de aprender sobre Python y SQL, mejorarás aspectos relacionados con desarrollo profesional: 
+habilidades para trabajar en equipo, pautas para una comunicación efectiva, gestión de la marca personal, integración en la comunidad tecnológica... 
+Lo primero en lo que profundizaremos es precisamente en Agile.
+
+¿Qué es Agile?
+En contra de lo que mucha gente cree, Agile no es una metodología, es una cultura de trabajo con sus valores y sus principios, 
+una mentalidad de cara a proporcionar un enfoque que facilite la inspección y adaptación en tiempos de incertidumbre.
+
+Agile (desarrollo ágil de software) nace en 2001 en EEUU de la mano de varios profesionales del sector que querían dejar plasmado en un manifiesto los valores imprescindibles para el desarrollo de software 
+de la forma más eficiente y satisfactoria posible. Merece la pena leerlo completo, tanto los valores como los principios: Manifiesto Agile.
+
+Esta fue carta presentación de Agile y lo llamaron el manifiesto para el desarrollo ágil de Software (SW). 
+Lo que nos proponían los firmantes con este manifiesto era empezar valorar y dar más importancia a los elementos de la izquierda frente a los de la derecha. Veámoslos uno a uno:
+
+Individuos e interacciones sobre procesos y herramientas: las personas son el centro y donde la comunicación suele ser la cable, y no tanto seguir procesos predefinidos o encorsetarnos en herramientas, 
+que es lo que haríamos si Agile fuese una metodología.
+
+SW funcionando sobre documentación: SW funcionando cuanto antes, para fallar pronto y detectar las cosas con antelación, más allá de tener documentación exhaustiva porque siempre será mejor ver una demostración 
+de cómo funciona algo y hacerse una idea de cómo queda que tener que leerse un manual de 200 hojas.
+
+Colaboración con el cliente sobre firmar un contrato: colaboración con el cliente, para poder contar su feedback temprano y saber si vamos por buen camino, por encima de negociar un contrato con términos y condiciones 
+que cumplir a largo plazo.
+
+Responder al cambio sobre seguir un plan: adaptación al cambio frente a seguir un plan medido y permanecer impasible.
+
+El sector tecnológico es altamente cambiante, con muchísima incertidumbre, lo que solemos llamar VUCA, donde la capacidad de adaptación es crucial y la palanca del cambio se encuentra en las personas. 
+Más allá de la complejidad de la situación que rodee la iniciativa en la que trabajemos haciendo SW, si priorizamos la visión, el entendimiento, la claridad y la agilidad como medio por encima de ese posible caos, 
+seremos más capaces de trabajar en entornos impredecibles. Cuanta más gente colabore para buscar ese entendimiento, más fácil será encontrar soluciones, por eso Agile es una cultura que concierne a toda la organización, 
+de forma que todo el mundo avance en el mismo sentido, con los mismos objetivos. Y, asumiendo que parte de esa complejidad no solo es inherente al contexto sino que también la añadimos las propias personas que estamos 
+detrás del SW, será de vital importancia el desarrollo de habilidades o soft-skills para poder responder antes estas situaciones.
+
+Las organizaciones ágiles y receptivas están diseñadas para aprender y responder rápidamente gracias a un feedback constante, un flujo de comunicación y colaboración clave para seguir evolucionando. 
+En ellas se anima a experimentar y aprender en ciclos rápidos, auto-organizados en equipos, que lideran los avances y hacen crecer la empresa. 
+Esto está directamente relacionado con los valores y los principios de la “cultura Agile” patente en el manifiesto, un mindset que muchas empresas del sector tecnológico ya han incorporado como parte de su ADN.
+
+Resumiendo, Agile es una cultura que trasciende a toda la organización, que está descrita por los 4 valores del manifiesto y con una serie de principios que giran en torno a ellos. 
+Todo esto se aterriza a través de un número ilimitado de prácticas como Design Thinking, Kanban, eXtreme Programming (XP) y Scrum que ayudan a su consecución.
+
+
+Para ello, hablaremos primero del conocido Ciclo de Deming, el Plan Do Check Act (PDCA): planifica, haz, comprueba y actúa, y vuelta a empezar. Edward Deming fue un reputado estadista del siglo pasado que introdujo, mucho antes del nacimiento de Agile, la utilidad de trabajar de forma cíclica. Él ya se aventuraba a asegurar que trabajar mediante ciclos de inspección y adaptación era una forma eficaz de ir aprendiendo.
+
+Y, ¿cómo funciona? Pues la idea es planificar qué hacer durante un periodo corto, a continuación comenzar hacerlo, y cuando acabe, medir qué tal ha salido para, finalmente, valorar los puntos de acción sobre el plan para ir mejorando y volver a iterar. De hecho, la teoría empírica o método científico basado en la experimentación se basa en esto. Así, el típico "prueba y error" podría corresponderse con los ciclos de Deming que se repetirían una y otra vez hasta ir alcanzando la mejor aproximación. Y aquí es donde entra en juego Scrum.
+
+Scrum nace como un marco de prácticas ágiles orientadas al desarrollo. Scrum establece objetivos a corto plazo y, sistemáticamente, paso a paso, va encontrando la manera de llegar a ellos a través de una serie de eventos y de roles. Es lo que se llama un proceso iterativo e incremental, acorde al Ciclo de Deming anteriormente mencionado y basándose en pequeños ciclos de desarrollo, que serán cortos periodos de tiempo previamente establecido, fijo y acotado que llamaremos Sprints. Pero no os adelanteremos nada más... tendréis que leer la siguiente guía para profundizar en cómo vamos a hacerlo.
+
+¿Por qué Agile en Adalab?
+Introducir Agile en el momento en el momento previo a comenzar la formación es clave ya que permite dotar de información y herramientas para una mejor gestión del nuevo contexto que, como ya hemos comentado, es bastante cambiante. Y, por ello, queremos que las alumnas de Adalab entiendan el porqué de este mindset desde el primer día. Así, gracias a habilidades como trabajar en equipo, comunicarse de forma efectiva, la tolerancia al fallo, etc. os convertiréis en grandísimas profesionales desde el primer momento, facilitando vuestra incorporación a cualquier empresa tecnológica, comprendiendo la cultura organizativa y adaptándose al nuevo entorno de forma más eficiente, lo cual marcará la diferencia respecto a otros perfiles juniors.
+
+Además, el propio Adalab se centra en esa cultura de empresa. Prueba de ello es que la gestión de Adalab responde a este concepto: se busca la mejora constante a través del feedback de las alumnas, profesores/as, facilitadores/as, voluntarios/as, empresas colaboradoras… para ir adaptando la formación a lo realmente demandado, poniendo foco en las personas que lo hacen posible y que aportan un valor añadido para crear un bootcamp diferenciador como este.
+
+Para saber cómo lo trabajaremos en Adalab, podéis seguir con la siguiente sección de la guía de Scrum donde lo contaremos en detalle.
+
+'''
